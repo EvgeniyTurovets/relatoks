758bd101253c361154c1af75c425dbe8e542a043
Antonster <Antontitov1993@gmail.com>
2021-09-29 18:49:43 +0300

    added news detailed page

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // слайдеры для главной
-  const mainSlider = tns({
-    container: '.main-slider',
-    items: 1,
-    nav: false,
-    speed: 600,
-    prevButton: '.main-slider-button-prev',
-    nextButton: '.main-slider-button-next'
-  });
+  if(document.querySelector('.main-slider')) {
+    const mainSlider = tns({
+      container: '.main-slider',
+      items: 1,
+      nav: false,
+      speed: 600,
+      prevButton: '.main-slider-button-prev',
+      nextButton: '.main-slider-button-next'
+    });
+  }
 
   // const resultSlider = tns({
   //   container: '.result-slider',
@@ -21,29 +23,46 @@ document.addEventListener('DOMContentLoaded', function() {
   //   nextButton: '.result-slider-button-next'
   // });
 
-  const newsSlider = tns({
-    container: '.news-slider',
-    fixedWidth: 440,
-    nav: false,
-    rewind: true,
-    loop: false,
-    slideBy: 3,
-    gutter: 30,
-    speed: 600,
-    prevButton: '.news-slider-button-prev',
-    nextButton: '.news-slider-button-next'
-  });
+  if(document.querySelector('.news-slider')) {
+    const newsSlider = tns({
+      container: '.news-slider',
+      fixedWidth: 440,
+      nav: false,
+      rewind: true,
+      loop: false,
+      slideBy: 3,
+      gutter: 30,
+      speed: 600,
+      prevButton: '.news-slider-button-prev',
+      nextButton: '.news-slider-button-next'
+    });
+  }
 
-  const aboutSlider = tns({
-    container: '.about-slider',
-    fixedWidth: 1410,
-    nav: false,
-    gutter: 400,
-    speed: 600,
-    prevButton: '.about-slider-button-prev',
-    nextButton: '.about-slider-button-next'
-  });
+
+  if(document.querySelector('.about-slider')) {
+    const aboutSlider = tns({
+      container: '.about-slider',
+      fixedWidth: 1410,
+      nav: false,
+      gutter: 400,
+      speed: 600,
+      prevButton: '.about-slider-button-prev',
+      nextButton: '.about-slider-button-next'
+    });
+  }
 
   // const element = document.getElementById("image-compare");
   // const viewer = new ImageCompare(element).mount();
+
+  // слайдер для детальных новостей
+  if(document.querySelector('.news-detailed-slider')) {
+    const newsDetailedSlider = tns({
+      container: '.news-detailed-slider',
+      nav: false,
+      gutter: 100,
+      speed: 600,
+      prevButton: '.news-detailed-slider-button-prev',
+      nextButton: '.news-detailed-slider-button-next'
+    });
+  }
 });
