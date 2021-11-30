8ea0703aa3c158ee00f77bc0f174f03e7d2ac81b
EvgeniyTurovets <websafrasoft@gmail.com>
2021-11-30 12:39:01 +0200

    1
    1

---
+++ app/js/main.js
@@ -0,0 +1,311 @@
+"use strict";
+
+$(document).ready(function () {
+  // общие функции
+  var sliderCounter = function sliderCounter(slider, selector) {
+    var count = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1;
+    var info = slider.getInfo();
+    document.querySelector(selector).innerHTML = info.slideCount <= count ? '' : "".concat(info.displayIndex, "/").concat(info.slideCount);
+    slider.events.on('indexChanged', function () {
+      var newInfo = slider.getInfo();
+      document.querySelector(selector).innerHTML = "".concat(newInfo.displayIndex, "/").concat(newInfo.slideCount);
+    });
+  }; // бургер с меню
+
+
+  if (document.getElementById('burger-checkbox')) {
+    var burger = document.getElementById('burger-checkbox');
+    var menu = document.querySelector('.header .menu');
+    var body = document.querySelector('body');
+
+    burger.onchange = function () {
+      if (burger.checked) {
+        menu.style.left = '0';
+        body.style.overflow = 'hidden';
+      } else {
+        menu.style.left = '100vw';
+        body.style.overflow = 'auto';
+      }
+    };
+
+    var menuItems = document.querySelectorAll('.header .menu .item');
+    menuItems.forEach(function (element) {
+      if (element.querySelector('.dropdown-content')) {
+        var dropdown = element.querySelector('.dropdown-content');
+        var buttonLeft = element.querySelector('.item-button');
+
+        buttonLeft.onclick = function () {
+          dropdown.style.left = '0';
+        };
+
+        var buttonRight = dropdown.querySelector('.back-button');
+
+        buttonRight.onclick = function () {
+          dropdown.style.left = '100vw';
+        };
+      }
+    });
+  }
+
+  if (document.querySelector('.main-slider')) {
+    var mainSlider = tns({
+      container: '.main-slider',
+      items: 1,
+      nav: false,
+      speed: 600,
+      prevButton: '.main-slider-button-prev',
+      nextButton: '.main-slider-button-next'
+    });
+  }
+
+  if (document.querySelector('.result-slider')) {
+    var resultSlider = tns({
+      container: '.result-slider',
+      touch: false,
+      swipeAngle: false,
+      fixedWidth: 330,
+      center: true,
+      nav: false,
+      speed: 600,
+      gutter: 200,
+      responsive: {
+        1410: {
+          fixedWidth: 700,
+          gutter: 150
+        }
+      },
+      prevButton: '.result-slider-button-prev',
+      nextButton: '.result-slider-button-next'
+    });
+    sliderCounter(resultSlider, '.result .count');
+    var slideList = document.querySelectorAll('.result-slider .slide');
+    slideList.forEach(function (element) {
+      var range = element.querySelector('.range');
+      var beforeSlide = element.querySelector('.before');
+      var rangeButton = element.querySelector('.range-button');
+
+      var changeFunc = function changeFunc(e) {
+        var rangePos = e.target.value;
+        beforeSlide.style.width = "".concat(rangePos, "%");
+
+        if (rangePos >= 98) {
+          beforeSlide.style.borderRadius = '20px';
+        } else {
+          beforeSlide.style.borderRadius = '20px 0 0 20px';
+        }
+
+        rangeButton.style.left = "calc(".concat(rangePos, "% - 28px)");
+      };
+
+      range.onchange = changeFunc;
+      range.oninput = changeFunc;
+    });
+  }
+
+  if (document.querySelector('.news-slider')) {
+    var newsSlider = tns({
+      container: '.news-slider',
+      nav: false,
+      rewind: true,
+      loop: false,
+      gutter: 30,
+      speed: 600,
+      responsive: {
+        1410: {
+          fixedWidth: 440
+        }
+      },
+      prevButton: '.news-slider-button-prev',
+      nextButton: '.news-slider-button-next'
+    });
+    sliderCounter(newsSlider, '.actual-news .count', 3);
+  }
+
+  if (document.querySelector('.awards-slider')) {
+    var awardsSlider = tns({
+      container: '.awards-slider',
+      nav: false,
+      controls: false,
+      fixedWidth: 240,
+      speed: 600,
+      gutter: 20,
+      loop: false,
+      center: true,
+      responsive: {
+        1410: {
+          fixedWidth: 300,
+          gutter: 150,
+          center: false
+        }
+      }
+    });
+  }
+
+  if (document.querySelector('.about-slider')) {
+    var aboutSlider = tns({
+      container: '.about-slider',
+      nav: false,
+      speed: 600,
+      gutter: 100,
+      responsive: {
+        1410: {
+          fixedWidth: 1410,
+          gutter: 400
+        }
+      },
+      prevButton: '.about-slider-button-prev',
+      nextButton: '.about-slider-button-next'
+    });
+    sliderCounter(aboutSlider, '.about .count');
+  } // слайдер для детальных новостей
+
+
+  if (document.querySelector('.news-detailed-slider')) {
+    var newsDetailedSlider = tns({
+      container: '.news-detailed-slider',
+      touch: false,
+      nav: false,
+      gutter: 100,
+      speed: 600,
+      prevButton: '.news-detailed-slider-button-prev',
+      nextButton: '.news-detailed-slider-button-next'
+    });
+  } // слайдеры для инструкций
+
+
+  if (document.querySelector('.instruction-slider')) {
+    var instructionSlider = tns({
+      container: '.instruction-slider',
+      nav: false,
+      gutter: 100,
+      speed: 600,
+      prevButton: '.instruction-slider-button-prev',
+      nextButton: '.instruction-slider-button-next'
+    });
+  }
+
+  if (document.querySelector('.documents-slider')) {
+    var documentsSlider = tns({
+      container: '.documents-slider',
+      fixedWidth: 210,
+      loop: false,
+      nav: false,
+      gutter: 30,
+      speed: 600,
+      prevButton: '.documents-slider-button-prev',
+      nextButton: '.documents-slider-button-next'
+    });
+    sliderCounter(documentsSlider, '.instruction .count', 4);
+  } // слайдер для производства
+
+
+  if (document.querySelector('.production-slider')) {
+    var _documentsSlider = tns({
+      container: '.production-slider',
+      loop: false,
+      nav: false,
+      controls: false,
+      gutter: 10,
+      speed: 600,
+      responsive: {
+        1410: {
+          fixedWidth: 340
+        }
+      }
+    });
+  } // калькулятор
+
+
+  if (document.getElementById('calculator-page')) {
+    var cosmetology = document.getElementById('calculate-cosmetology');
+    var neurology = document.getElementById('calculate-neurology');
+    var line = document.getElementById('calculator-title-line');
+    var cosmetologyInfo = document.getElementById('cosmetology-info');
+    var neurologyInfo = document.getElementById('neurology-info'); // клики по чекбоксам косметологии и неврологии
+
+    cosmetology.onclick = function () {
+      cosmetology.classList.add('active');
+      neurology.classList.remove('active');
+      line.className = 'title-line-pink';
+      cosmetologyInfo.classList.add('active');
+      neurologyInfo.classList.remove('active');
+    };
+
+    neurology.onclick = function () {
+      neurology.classList.add('active');
+      cosmetology.classList.remove('active');
+      line.className = 'title-line-blue';
+      neurologyInfo.classList.add('active');
+      cosmetologyInfo.classList.remove('active');
+    }; // выбор шприца
+
+
+    var injectorSelect = document.querySelector('.injector-select');
+    var ml1seg50 = document.querySelector('.ml1seg50');
+    var ml1seg40 = document.querySelector('.ml1seg40');
+    var ml05seg50 = document.querySelector('.ml05seg50');
+    var ml03seg30 = document.querySelector('.ml03seg30');
+
+    injectorSelect.onchange = function () {
+      var injectorId = injectorSelect.options[injectorSelect.selectedIndex].id;
+      ml1seg50.style.display = 'none';
+      ml1seg40.style.display = 'none';
+      ml05seg50.style.display = 'none';
+      ml03seg30.style.display = 'none';
+
+      switch (injectorId) {
+        case 'ml1seg50':
+          ml1seg50.style.display = 'block';
+          break;
+
+        case 'ml1seg40':
+          ml1seg40.style.display = 'block';
+          break;
+
+        case 'ml05seg50':
+          ml05seg50.style.display = 'block';
+          break;
+
+        case 'ml03seg30':
+          ml03seg30.style.display = 'block';
+          break;
+      }
+    }; // клик по кнопке завершения расчета в косметологии
+
+
+    var cosmetologyСalculationButton = document.getElementById('cosmetology-calculation-button');
+    var cosmetologyCalculationButtonContainer = document.getElementById('cosmetology-calculation-button-container');
+    var cosmetologyCalculationResult = document.getElementById('cosmetology-calculation-result');
+
+    cosmetologyСalculationButton.onclick = function () {
+      cosmetologyCalculationButtonContainer.classList.remove('active');
+      cosmetologyCalculationResult.classList.add('active');
+    }; // клик по кнопке завершения расчета в неврологии
+
+
+    var neurologyСalculationButton = document.getElementById('neurology-calculation-button');
+    var neurologyCalculationButtonContainer = document.getElementById('neurology-calculation-button-container');
+    var neurologyCalculationResult = document.getElementById('neurology-calculation-result');
+
+    neurologyСalculationButton.onclick = function () {
+      neurologyCalculationButtonContainer.classList.remove('active');
+      neurologyCalculationResult.classList.add('active');
+    }; // кастомные селекты
+
+
+    var calculatorSelectTemplate = {
+      minimumResultsForSearch: Infinity,
+      width: '100%',
+      theme: 'relatox_gray',
+      dropdownPosition: 'below'
+    };
+    $('.injector-select').select2(calculatorSelectTemplate);
+    $('.dose-select').select2(calculatorSelectTemplate);
+    $('.indications-select').select2(calculatorSelectTemplate);
+    $('.age-select').select2(calculatorSelectTemplate);
+    $('.part-select').select2(calculatorSelectTemplate);
+    $('.muscle-select').select2(calculatorSelectTemplate);
+    $('.mas-select').select2(calculatorSelectTemplate);
+  }
+});
+//# sourceMappingURL=main.js.map
